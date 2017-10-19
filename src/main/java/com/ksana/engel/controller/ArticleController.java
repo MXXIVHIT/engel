1222aa5e5fc744bd36cdbe7eb9cf11297c6d083e
chaos <xianchaox@gmail.com>
2017-10-19 15:59:21 +0800

    first commit

---
+++ src/main/java/com/ksana/engel/controller/ArticleController.java
@@ -0,0 +1,21 @@
+package com.ksana.engel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ArticleController {
+
+    /*@GetMapping()
+    public String index(){
+
+        return "index";
+    }*/
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
